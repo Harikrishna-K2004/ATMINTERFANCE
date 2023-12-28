bf7dd7d77c88cf572c0db8f73dc345d20570702d
HARIKRISHNAK3539 <harikrishnak.it2023@citchennai.net>
2023-12-28 15:49:46 +0530

    spring boot first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 import java.util.*;
-class ATMCLASS {
-    protected double accountbalance=0 ;
-    public double balance  ;
 
-    public void setAccountBalance(int acc) {
-        balance = acc;
-        accountbalance += balance;
+class Transaction {
+    protected String type ;
+    protected double transaction_amount;
+
+    public Transaction(String type ,double transaction_amount){
+        this.type = type;
+        this.transaction_amount = transaction_amount;
+    }
+}
+class ATMCLASS {
+    public ArrayList<Transaction> transactionsHistory;
+    protected double accountbalance=0 ;
+    protected double balance  ;
+    protected double withdrawamount;
+    protected double depositamount;
+    protected double remainingamount;
+    public ATMCLASS(){
+        this.balance = 0.0 ;
+        this.transactionsHistory = new ArrayList<>();
     }
 
-}
-class userbankaccount extends ATMCLASS{
-    public double withdrawamount;
-    public double depositamount;
-    public double remainingamount;
     public void withdraw(double amount1){
         withdrawamount = amount1;
         if(withdrawamount  <= accountbalance){
@@ -20,6 +28,7 @@ class userbankaccount extends ATMCLASS{
             System.out.println("    Congratulations, your transaction of $"+withdrawamount+" is successful.");
             accountbalance = accountbalance - withdrawamount;
             System.out.println("    Your remaining Account Balance is : $"+ accountbalance);
+            transactionsHistory.add(new Transaction("Withdraw", amount1));
         }
         else if(withdrawamount > accountbalance){
             System.out.println("    Insufficient Balance for transaction, Please select the amount within $"+accountbalance);
@@ -33,6 +42,7 @@ class userbankaccount extends ATMCLASS{
             accountbalance += depositamount;
             System.out.println("      Thanks for deposting $" + depositamount + " in your account");
             System.out.println("      Now your account balance is $" + accountbalance);
+            transactionsHistory.add(new Transaction("Deposit", deposit));
         }
         else{
             System.out.println("     The maximum deposit amount must within $20000000.");
@@ -42,54 +52,60 @@ class userbankaccount extends ATMCLASS{
     public void checkbalance(){
         System.out.println("    Your account's current balance is $"+accountbalance);
     }
+    public ArrayList<Transaction> showTransactionHistory() {
+        return transactionsHistory;
+    }
 }
 public class Main extends ATMCLASS{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ATMCLASS atm = new ATMCLASS();
-        userbankaccount user = new userbankaccount();
+        ATMCLASS user = new ATMCLASS();
         boolean condition = true;
         System.out.println();
         System.out.println();
         System.out.println("*** WELCOME TO ONLINE BANK ACCOUNT *** ");
         while(condition == true) {
             System.out.println();
-            System.out.println(" Press 1 : To deposit amount ,");
-            System.out.println(" Press 2 : To withdraw amount ,");
-            System.out.println(" Press 3 : To check account balance,");
-            System.out.println("Press 0 : Exit ");
+            System.out.println("  1 => To Deposit amount ,");
+            System.out.println("  2 => To Withdraw amount ,");
+            System.out.println("  3 => To Check Account Balance,");
+            System.out.println("  4 => To see Transaction History, ");
+            System.out.println("  0 => Exit ");
             System.out.println();
             System.out.print("   Your wish here :");
             int i = sc.nextInt();
             System.out.println();
-            while (i == 1) {
+            if (i == 1) {
                 System.out.print("   Enter the amount that you want to deposit : $ ");
-                double deposit_amount = sc.nextInt();
+                double deposit_amount = sc.nextDouble();
                 user.deposit(deposit_amount);
                 System.out.println();
-                break;
             }
 
-            while (i == 2) {
+            if (i == 2) {
                 System.out.print("     Enter the amount that you want to withdraw : $ ");
-                double withdraw_amount = sc.nextInt();
+                double withdraw_amount = sc.nextDouble();
                 user.withdraw(withdraw_amount);
                 System.out.println();
-                break;
             }
 
-            while (i == 3) {
+            if (i == 3) {
                 System.out.println("     Wait we are fetching your Account balance!");
                 System.out.println();
                 user.checkbalance();
-                break;
+            }
+             if (i==4){
+                ArrayList<Transaction> transactionsamount = user.showTransactionHistory();
+                System.out.println("Transaction History:");
+                for (Transaction transaction : transactionsamount) {
+                    System.out.println(transaction.type + ": $" + transaction.transaction_amount);
+                }
             }
 
-            while (i == 0) {
+            if (i == 0) {
                 condition=false;
                 System.out.println("    Thank you,Welcome again !");
                 System.out.println();
-                break;
             }
 
         }
